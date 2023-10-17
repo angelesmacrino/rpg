921b0186bb027007d67ffd46d6fd917964765a15
angeles <carolinamacrino@gmail.com>
2023-10-17 13:16:27 -0300

    bootstrap test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+
 
 import './assets/main.css'
 
